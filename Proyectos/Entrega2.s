45905952e92f405266d2b63c1b17d585b6f72114
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2017-11-01 11:38:48 +0100

    Update Entrega2.s

--- Proyectos/Entrega2.s
+++ Proyectos/Entrega2.s
@@ -1,7 +1,19 @@
 #Ricardo Meza Díaz 
 #Entrega de práctica,Jueves 21 Septiembre
-#Ejercicio 2
+#Ejercicio 2,operaciones básicas con la memoria.
 
-    .data       
-    .asciiz "Hola Mundo
+#Realiza un programa en ensamblador del MIPS que cargue en la memoria,
+#consecutivamente, los siguientes datos:
+# Los siguientes bytes: 16, 32, 48.
+# Las siguientes medias palabras: 16, 32, 48.
+# Las siguientes palabras:16, 32, 48.
+#Carga el programa en el simulador e interpretar el contenido de las direcciones de memoria.
+ 
+    .data                   #Los elementos siguientes son almacenados en el segmento de datos.   
+
+    .byte	16,32,48         #Almacena los n valores de 8 bits en bytes consecutivas de memoria.
+    .half	16,32,48          #Almacena los n valores de 16 bits en medias palabras consecutivas de memoria.
+    .word	16,32,48          #Almacena los n valores de 32 bits en palabras consecutivas de memoria.
+
+    .text                    #Los elementos siguientes son almacenados en el segmento de texto.Estos elementos solo pueden ser instrucciones o palabras.
 
