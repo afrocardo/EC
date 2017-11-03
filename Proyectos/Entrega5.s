df8e0f41d6b633cfcd71a747a2d19fd8dcc5390c
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2017-11-04 00:15:33 +0100

    Update Entrega5.s

--- Proyectos/Entrega5.s
+++ Proyectos/Entrega5.s
@@ -1,1 +1,13 @@
+#Ricardo Meza Díaz 
+#Entrega de práctica,Jueves 21 Septiembre
+#Ejercicio 5,modos de direccionamiento.
+
+#Realizar un programa en ensamblador del MIPS que cargue el contenido de una posición de
+#memoria en un registro y luego escriba el valor del registro en consola. Debe repetir esta
+#operación para cada uno de los 7 modos de direccionamiento existentes en el MIPS
+#(estudiar la transparencia nº 10 del tutorial). Comprobar que en la consola aparece siempre
+#el mismo valor (pues son modos distintos de acceder a un mismo dato). Se recomienda
+#enseñar al profesor el código desarrollado, con el objeto de identificar correctamente cada
+#modo de direccionamiento.
+
 
