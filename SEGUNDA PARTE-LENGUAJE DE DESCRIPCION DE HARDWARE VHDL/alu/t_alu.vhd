16b220628e746343b01ecfd2ee3285debe62cac2
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2017-12-29 17:28:01 +0100

    Update t_alu.vhd

--- SEGUNDA PARTE-LENGUAJE DE DESCRIPCION DE HARDWARE VHDL/alu/t_alu.vhd
+++ SEGUNDA PARTE-LENGUAJE DE DESCRIPCION DE HARDWARE VHDL/alu/t_alu.vhd
@@ -11,7 +11,7 @@ architecture estimulos of t_alu is
   signal a, b, salida : integer;
   signal operacion : bit;
 begin
-  dut : entity work.alu(alu_arch) port map (a, b, operacion, salida);
+  dut : entity work.alu(flujo) port map (a, b, operacion, salida);
   process is
   begin
     a <= 0; b <= 0; operacion <= '0'; wait for 20 ns;
